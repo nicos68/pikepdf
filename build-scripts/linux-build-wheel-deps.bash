33f9587adeaf03fa9e92324464a9696b5acb413b
James R. Barlow <james@purplerock.ca>
2021-04-14 02:22:28 -0700

    try patching qpdf
    This seems to be a dead end. QPDF for aarch64 takes too much memory to build on QEMU+Docker+Github Actions.

--- build-scripts/linux-build-wheel-deps.bash
+++ build-scripts/linux-build-wheel-deps.bash
@@ -15,7 +15,37 @@ fi
 
 if [ ! -f /usr/local/lib/libqpdf.a ]; then
     pushd qpdf
-    ./configure --disable-oss-fuzz && make -j install
+    patch -p1 --ignore-whitespace --unified << 'INSTALL_LIBS'
+diff --git a/make/libtool.mk b/make/libtool.mk
+index a259afe0..6b4276b3 100644
+--- a/make/libtool.mk
++++ b/make/libtool.mk
+@@ -106,6 +106,17 @@ endef
+
+ # Install target
+
++install-libs: build_libqpdf
++	./mkinstalldirs -m 0755 $(DESTDIR)$(libdir)/pkgconfig
++	./mkinstalldirs -m 0755 $(DESTDIR)$(includedir)/qpdf
++	$(LIBTOOL) --mode=install ./install-sh \
++		libqpdf/$(OUTPUT_DIR)/libqpdf.la \
++		$(DESTDIR)$(libdir)/libqpdf.la
++	$(LIBTOOL) --finish $(DESTDIR)$(libdir)
++	./install-sh -m 0644 include/qpdf/*.h $(DESTDIR)$(includedir)/qpdf
++	./install-sh -m 0644 include/qpdf/*.hh $(DESTDIR)$(includedir)/qpdf
++	./install-sh -m 0644 libqpdf.pc $(DESTDIR)$(libdir)/pkgconfig
++
+ # NOTE: If installing any new executables, remember to update the
+ # lambda layer code in build-scripts/build-appimage.
+ install: all
++
+INSTALL_LIBS
+
+    ./configure \
+        --disable-oss-fuzz \
+        --disable-doc-maintenance \
+        --disable-html-doc --disable-pdf-doc --disable-validate-doc \
+    && make -j install-libs
     find /usr/local/lib -name 'libqpdf.so*' -type f -exec strip --strip-debug {} \+
     popd
 fi
